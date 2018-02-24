fba3e331715213780cef05ab3b7bd8d9a1fdbf33
Sun Davey <SunDavey@hotmail.com>
2018-02-23 16:38:39 -0800

    Initial commit

---
+++ WebFundamentals/CSS/periodic_table/periodic.js
@@ -0,0 +1,12 @@
+
+var num=[];
+for(var i = 1; i < 103; i++){
+    num.push(i);
+}
+var name=["H", "He", "Li", "Be", "B", "C", "N", "O","F", "NE", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V",
+            "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", 
+            "Ru", "Rh", "Rh", "Pd", "Ag", "Cd","In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", 
+            "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", 
+            "Cn", "Uut", "Fl", "Uup", "Lv", "Uss", "Uuo", "La", "Ce", "Pr", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Ac", 
+            "Th", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No"];
+var weight=[];
